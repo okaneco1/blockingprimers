3f1bcf7c33a71c2f7f97b889c3167684e2533185
okaneco1 <okaneco1@msu.edu>
2024-04-18 19:46:27 -0400

    added simplified plot for poster

--- sequence_reads_per_taxa_per_blocker.R
+++ sequence_reads_per_taxa_per_blocker.R
@@ -23,6 +23,10 @@ matrix$Sample <- gsub("\\.12S$", "", matrix$Sample) #simplify sample names
 # these are lake trout, white sucker, sea lamprey, and Salmonidae 
 matrix1 <- matrix[,1:5]
 
+# filter out 25 cycles
+matrix1 <- matrix1 %>%
+  filter(grepl("40_", Sample))
+
 # which sample?
 matrix1 <- matrix1 %>%
   mutate(sample = case_when(
@@ -50,8 +54,14 @@ matrix1 <- matrix1 %>%
     TRUE ~ "No_Blocker" # default case if none of the above conditions are met
   ))
 
-# reorder to sort by blocker 
+# reorder to sort by blocker, and to start with no blocker
 matrix1 <- matrix1[order(matrix1$specific_blocker), ]
+matrix1 <- matrix1 %>%
+  mutate(specific_blocker = factor(specific_blocker, levels = c(
+    "No_Blocker", "Blocker1", "Blocker2", "Blocker3", "Blocker4",
+    "Blocker5", "Blocker6", "Blocker7", "Blocker8"
+  )))
+
 
 # pivot longer to prepare for stacked bar chart
 matrix1_long <- matrix1 %>%
@@ -109,11 +119,54 @@ matrix1_long_plot <- ggplot(matrix1_long, aes(x = specific_blocker, y = Reads, f
         strip.text = element_text(size = 14),
         axis.text = element_text(family = "Helvetica", face = "plain"),
         axis.text.x = element_text(size = 9),
-        legend.key.size = unit(1.5, "lines"),  # Adjust legend key size
-        legend.text = element_text(size = 12),  # Adjust legend text size
+        legend.key.size = unit(1.5, "lines"),  
+        legend.text = element_text(size = 12),  
         legend.title = element_text(size = 14, face = "bold"))
 
 matrix1_long_plot
+
+#-------------------------------------------------------------------------------
+# Simplified plot for poster presentation
+
+# only looking to plot M4, M5, HP15, and CA14
+matrix1_simple <- matrix1_long %>%
+  filter(Digestive_Sample %in% c("M4", "M5", "HP15", "CA14"))
+
+#drop unused levels
+matrix1_simple$Digestive_Sample <- droplevels(matrix1_simple$Digestive_Sample)
+
+# change facet labels for clarity
+levels(matrix1_simple$Digestive_Sample) <- c("M4 (90:10 SL:LT)",
+                                             "M5 (90:10 SL:WS)",
+                                             "HP15 (Wild Juvenile)",
+                                             "CA14 (Wild Adult)")
+
+# plot as before
+matrix1_simple_plot <- ggplot(matrix1_simple, aes(x = specific_blocker, y = Reads, fill = Species)) +
+  geom_col() +
+  scale_fill_manual(values = custom_colors, 
+                    labels = c("Salmonid",  "Lake Trout", "White Sucker", "Sea Lamprey"))+
+  theme_few()+
+  theme(axis.text.x = element_text(angle = 60, vjust = 1, hjust = 1, size = 15),
+        plot.title = element_text(hjust = 0.5)) +
+  ggtitle("Sequence Read Counts per Taxa per Blocking Primer") +
+  labs(x = "Blocking Primer", y = "Number of Sequence Reads") +
+  facet_wrap(~ Digestive_Sample, ncol = 2)+  # toggle this on/off for wrapped/unwrapped plot
+  theme(text = element_text(family = "Helvetica", face = "bold"),
+        strip.text = element_text(size = 14),
+        axis.text = element_text(family = "Helvetica", face = "plain"),
+        axis.text.x = element_text(size = 9),
+        legend.key.size = unit(1.25, "lines"),  
+        legend.text = element_text(size = 11),  
+        #legend.title = element_text(size = 14, face = "bold"),
+        legend.title = element_blank(),
+        legend.position = "bottom"  
+  ) +
+  guides(fill = guide_legend(nrow = 1, byrow = TRUE))
+
+matrix1_simple_plot
+
+
 
 #-------------------------------------------------------------------------------
 # Statistical comparisons
